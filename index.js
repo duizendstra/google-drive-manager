9316a1db2f635a44f411ff07f70fad6a50a896cd
Jasper Duizendstra <jasper.duizendstra@g-workplace.com>
2016-11-27 09:03:06 +0100

    added createfile

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function googleDriveManager(mainSpecs) {
             var request = {
                 auth: auth,
                 pageSize: 500
-             };
+            };
 
             if (specs.q) {
                 request.q = specs.q;
@@ -57,9 +57,180 @@ function googleDriveManager(mainSpecs) {
         });
     }
 
+    function createFile(specs) {
+        return new Promise(function (resolve, reject) {
+            var name = specs.name;
+            var mimeType = specs.mimeType;
+            var parents = specs.parents;
+            var request = {
+                auth: auth,
+                resource: {
+                    name: name,
+                    mimeType: mimeType,
+                    parents: parents
+                }
+            };
+
+            if (specs.fields) {
+                request.fields = specs.fields;
+            }
+
+            service.files.create(request, function (err, response) {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(response);
+            });
+        });
+    }
+
+    function copy(specs) {
+
+        return new Promise(function (resolve, reject) {
+            var fileId = specs.fileId;
+            var request = {
+                auth: auth,
+                fileId: fileId
+            };
+
+            if (specs.fields) {
+                request.fields = specs.fields;
+            }
+
+            service.files.copy(request, function (err, response) {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(response);
+            });
+        });
+    }
+
+    function addParents(specs) {
+        return new Promise(function (resolve, reject) {
+            var fileId = specs.fileId;
+            var newParents = specs.newParents;
+            var request = {
+                auth: auth,
+                fileId: fileId,
+                resource: {
+                    addParents: newParents
+                }
+            };
+
+            if (specs.fields) {
+                request.fields = specs.fields;
+            }
+
+            service.files.copy(request, function (err, response) {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(response);
+            });
+        });
+    }
+
+    function getPermissions(specs) {
+        return new Promise(function (resolve, reject) {
+            var fileId = specs.fileId;
+            var request = {
+                auth: auth,
+                fileId: fileId
+            };
+
+            if (specs.fields) {
+                request.fields = specs.fields;
+            }
+
+            service.permissions.list(request, function (err, response) {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(response);
+            });
+        });
+    }
+
+    function addPermission(specs) {
+        return new Promise(function (resolve, reject) {
+            var fileId = specs.fileId;
+            var permissionId = specs.permissionId;
+            var transferOwnership = specs.transferOwnership;
+            var role = specs.role;
+            var emailAddress = specs.emailAddress;
+            var type = specs.type;
+            var request = {
+                auth: auth,
+                fileId: fileId,
+                permissionId: permissionId,
+                transferOwnership: transferOwnership,
+                resource: {
+                    role: role,
+                    emailAddress: emailAddress,
+                    type: type
+                }
+            };
+
+            if (specs.fields) {
+                request.fields = specs.fields;
+            }
+
+            service.permissions.create(request, function (err, response) {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(response);
+            });
+        });
+    }
+
+
+    function updatePermission(specs) {
+        return new Promise(function (resolve, reject) {
+            var fileId = specs.fileId;
+            var permissionId = specs.permissionId;
+            var transferOwnership = specs.transferOwnership;
+            var role = specs.role;
+            var request = {
+                auth: auth,
+                fileId: fileId,
+                permissionId: permissionId,
+                transferOwnership: transferOwnership,
+                resource: {
+                    role: role
+                }
+            };
+
+            if (specs.fields) {
+                request.fields = specs.fields;
+            }
+
+            service.permissions.update(request, function (err, response) {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(response);
+            });
+        });
+    }
+
+
     auth = mainSpecs.auth;
     return {
-        getFiles: getFiles
+        getFiles: getFiles,
+        copy: copy,
+        getPermissions: getPermissions,
+        updatePermission: updatePermission,
+        addPermission: addPermission,
+        createFile: createFile,
+        addParents: addParents
     };
 }
 
